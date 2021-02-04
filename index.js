ac90839db0589e2f9eead5d9f808adb147cfa132
VictorLCosta <victorlc2019@outlook.com>
2021-02-04 17:38:18 -0300

    Capturing data with form

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json())
 
-app.get('/:name?', (req, res) => 
+app.get('/', (req, res) => 
 {
     res.render('index');
 });
 
-app.get('/questions/new', (req, res) =>
+app.get('/questions', (req, res) =>
 {
     res.render('question');
+});
+
+app.post('/questions/new', (req, res) =>
+{
+    var title = req.body.title;
+    var desc = req.body.desc;
+    res.send('Formulário recebido! ' + title + ': ' + desc);
 });
 
 app.listen(8080, (erro) =>
